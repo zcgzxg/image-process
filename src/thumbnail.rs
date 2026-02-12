31034461cd768d66712cb15ca9ee87e66179ffce
tn_zhuxg <1134495285@qq.com>
2026-02-12 14:46:59 +0800

    feat: image overlaying

--- src/thumbnail.rs
+++ src/thumbnail.rs
@@ -1,6 +1,6 @@
 use std::io::Cursor;
 
-use image::{DynamicImage, ImageFormat, ImageReader};
+use image::{DynamicImage, ImageFormat, ImageReader, imageops};
 use wasm_bindgen::prelude::*;
 
 const SUPPORTED_FORMATS: &[ImageFormat] = &[ImageFormat::Png, ImageFormat::Jpeg];
@@ -42,6 +42,22 @@ impl Image {
 
         let mut w = Cursor::new(Vec::new());
         thumbnail
+            .write_to(&mut w, self.format)
+            .map_err(|e| e.to_string())?;
+
+        Ok(w.into_inner())
+    }
+
+    #[wasm_bindgen]
+    pub fn overlaying(&mut self, top_image: &Image) -> Result<Vec<u8>, JsValue> {
+        let (wa, ha) = (self.image.width(), self.image.height());
+        let (target_w, target_h) = (((wa as f32) * 0.1) as u32, ((ha as f32) * 0.1) as u32);
+        let (x, y) = ((wa as f32) * 0.88, (ha as f32) * 0.88);
+
+        let thumbnail = top_image.image.thumbnail(target_w, target_h);
+        imageops::overlay(&mut self.image, &thumbnail, x as i64, y as i64);
+        let mut w = Cursor::new(Vec::new());
+        self.image
             .write_to(&mut w, self.format)
             .map_err(|e| e.to_string())?;
 
