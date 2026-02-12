d4b9d3774bee18b83031fce860333840ee35a704
tn_zhuxg <1134495285@qq.com>
2026-02-12 21:35:36 +0800

    feat: change build script

--- build-mp.sh
+++ build-mp.sh
@@ -1,11 +1,17 @@
-wasm-pack build \
-  --release \
-  --weak-refs \
-  --out-dir dist-mp \
-  --reference-types \
-  --target no-modules \
-  -- --no-default-features
+cargo build \
+    --target wasm32-unknown-unknown \
+    --release
+
+wasm-bindgen \
+    target/wasm32-unknown-unknown/release/image_process.wasm \
+    --out-dir dist-mp \
+    --target bundler \
+    --encode-into never \
+    --reference-types \
+    --weak-refs
 
 static-compress \
     -c brotli \
     ./dist-mp/*.wasm
+
+ls -lah ./dist-mp
--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
-wasm-pack build \
-  --release \
-  --weak-refs \
-  --out-dir dist-web \
-  --reference-types \
-  --target web \
-  -- --no-default-features
+cargo build \
+    --target wasm32-unknown-unknown \
+    --release
+
+wasm-bindgen \
+    target/wasm32-unknown-unknown/release/image_process.wasm \
+    --out-dir dist-web \
+    --target bundler \
+    --reference-types \
+    --weak-refs
 
 static-compress \
     -c brotli \
     ./dist-web/*.wasm
+
+ls -lah ./dist-web
