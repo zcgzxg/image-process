6e1de85ee60ad2660eda7a00d1773826f1228c16
tn_zhuxg <1134495285@qq.com>
2026-02-15 14:56:28 +0800

    feat: ast-grep replace dist-mp

--- build-mp.sh
+++ build-mp.sh
@@ -9,6 +9,10 @@ wasm-bindgen \
     --out-dir dist-mp \
     --target web
 
+# 使用 ast-grep 自动修改生成的代码以兼容小程序
+echo "正在修改生成的代码以兼容小程序..."
+sg scan -U ./dist-mp
+
 static-compress \
     -c brotli \
     ./dist-mp/*.wasm
