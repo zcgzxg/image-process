2cab454a8cdfdc0bbb00f9ce2d61ae47b584abe6
tn_zhuxg <1134495285@qq.com>
2026-02-21 23:10:24 +0800

    feat: enable SIMD, error handing

--- src/thumbnail.rs
+++ src/thumbnail.rs
@@ -16,29 +16,31 @@ pub struct Image {
 impl Image {
     /// 创建图片对象
     #[wasm_bindgen(constructor)]
-    pub fn new(image_data: Vec<u8>) -> Result<Self, JsValue> {
+    pub fn new(image_data: Vec<u8>) -> Result<Self, JsError> {
         let reader = Cursor::new(image_data);
-        let image = ImageReader::new(reader)
-            .with_guessed_format()
-            .map_err(|e| e.to_string())?;
+        let image = ImageReader::new(reader).with_guessed_format()?;
 
         let format = match image.format() {
             Some(format) => {
                 if !SUPPORTED_FORMATS.contains(&format) {
-                    return Err(JsValue::from(format!(
+                    return Err(JsError::new(&format!(
                         "Unsupported format: {}",
                         format.to_mime_type()
                     )));
                 }
                 format
             }
-            None => return Err(JsValue::from_str("Unsupported format None")),
+            None => return Err(JsError::new("Unsupported format None")),
         };
 
         Ok(Self {
-            image: image
-                .decode()
-                .map_err(|e| format!("err: {}, format: {}", e, format.to_mime_type()))?,
+            image: image.decode().map_err(|e| {
+                JsError::new(&format!(
+                    "err: {}, format: {}",
+                    e.to_string(),
+                    format.to_mime_type()
+                ))
+            })?,
             format,
         })
     }
@@ -51,19 +53,17 @@ impl Image {
 
     /// 生成缩略图
     #[wasm_bindgen]
-    pub fn thumbnail(&self, width: u32, height: u32) -> Result<Vec<u8>, JsValue> {
+    pub fn thumbnail(&self, width: u32, height: u32) -> Result<Vec<u8>, JsError> {
         let thumbnail = self.image.thumbnail(width, height);
 
         let mut w = Cursor::new(Vec::new());
-        thumbnail
-            .write_to(&mut w, self.format)
-            .map_err(|e| e.to_string())?;
+        thumbnail.write_to(&mut w, self.format)?;
 
         Ok(w.into_inner())
     }
 
     #[wasm_bindgen]
-    pub fn overlaying(&mut self, top_image: &Image) -> Result<Vec<u8>, JsValue> {
+    pub fn overlaying(&mut self, top_image: &Image) -> Result<Vec<u8>, JsError> {
         let (wa, ha) = (self.image.width(), self.image.height());
         let (target_w, target_h) = (((wa as f32) * 0.1) as u32, ((ha as f32) * 0.1) as u32);
         let (x, y) = ((wa as f32) * 0.88, (ha as f32) * 0.88);
@@ -71,9 +71,7 @@ impl Image {
         let thumbnail = top_image.image.thumbnail(target_w, target_h);
         imageops::overlay(&mut self.image, &thumbnail, x as i64, y as i64);
         let mut w = Cursor::new(Vec::new());
-        self.image
-            .write_to(&mut w, self.format)
-            .map_err(|e| e.to_string())?;
+        self.image.write_to(&mut w, self.format)?;
 
         Ok(w.into_inner())
     }
