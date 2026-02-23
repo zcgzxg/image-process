f6985ccf2854a305bd5749337dcf7b5d1d4fb3a3
tn_zhuxg <1134495285@qq.com>
2026-02-23 22:57:53 +0800

    feat: add wasm-opt

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 cargo +nightly build --release
 
+wasm-opt -O3 \
+    --enable-nontrapping-float-to-int \
+    --enable-bulk-memory \
+    --enable-simd \
+    --strip-debug \
+    --strip-producers \
+    target/wasm32-unknown-unknown/release/image_process.wasm \
+    -o target/wasm32-unknown-unknown/release/image_process.wasm
+
 wasm-bindgen \
     target/wasm32-unknown-unknown/release/image_process.wasm \
     --out-dir ./web/src/image-process \
